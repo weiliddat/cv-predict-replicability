7dfaeec8efacfb309f7cafed188fb082920d7fc9
ariannechr <52212216+ariannechr@users.noreply.github.com>
2019-06-26 19:01:07 +0200

    Update Analysis.R
    lines 6, 10, 19, 32
    I believe these changes are required to obtain correct values.

--- Ackerman et al (2010) (done)/Analysis.R
+++ Ackerman et al (2010) (done)/Analysis.R
@@ -3,9 +3,11 @@ library(dplyr)
 library(caret)
 
 data <- read_csv("Ackerman et al. (2010) - Replicaiton Data .csv")
+#to deal with imputed values, I believe, we created a new .csv file "Ackerman et al. (2010) - Updated_resolved.csv". 
 
 data %>%
   filter(Collection == 1) %>%
+  filter(Exc_resolved == 0) %>% #line updated by Arianne
   (function(x) {
     xh <- x %>% filter(Condition == "H") %>% select("DV") %>% as.data.frame()
     xl <- x %>% filter(Condition == "L") %>% select("DV") %>% as.data.frame()
@@ -14,6 +16,7 @@ data %>%
   }) -> test1
 
 data %>%
+  filter(Exc_resolved == 0) %>%  #line updated by Arianne
   (function(x) {
     xh <- x %>% filter(Condition == "H") %>% select("DV") %>% as.data.frame()
     xl <- x %>% filter(Condition == "L") %>% select("DV") %>% as.data.frame()
@@ -26,6 +29,7 @@ print(test1)
 print(test2)
 
 lm(DV ~ Condition, data) %>% summary()
+lm(DV ~ Condition, data[which(data$Exc_resolved == 0),]) %>% summary() #To account for the extra filter in lines 10 & 19.
 
 cvControl <- trainControl(
   method = "repeatedcv",
